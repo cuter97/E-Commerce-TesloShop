8b4e7b4f3d399116ed8075d74c8cd1b80775a222
cuter97 <aguscastets@gmail.com>
2024-01-28 18:23:05 -0300

    add components

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,4 +1,17 @@
-export default function () {
+import { notFound } from "next/navigation";
+
+interface Props {
+    params: {
+        id: string;
+    }
+}
+
+export default function ({ params }: Props) {
+
+    const { id } = params;
+
+    if (id === 'kids') notFound();
+
     return (
         <div>Category Page</div>
     )
