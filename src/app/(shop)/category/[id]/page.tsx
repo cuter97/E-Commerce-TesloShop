c0fa87627b7eb7a6d65a3b22a6ec4f987f081085
cuter97 <aguscastets@gmail.com>
2024-01-29 19:25:20 -0300

    add category and products components

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,18 +1,39 @@
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
+
+const seedProducts = initialData.products;
 
 interface Props {
     params: {
-        id: string;
+        id: Category;
     }
 }
 
 export default function ({ params }: Props) {
 
     const { id } = params;
+    const products = seedProducts.filter(prod => prod.gender === id);
+    const productId = seedProducts.find(prod => prod.gender === id);
 
-    if (id === 'kids') notFound();
+    const labels: Record<Category, string> = {
+        'men': 'para Hombres',
+        'women': 'para Mujeres',
+        'kid': 'para niños',
+        'unisex': 'para todos',
+    }
+
+    if (!productId) notFound();
 
     return (
-        <div>Category Page</div>
+        <>
+            <Title
+                title={`Articulos ${labels[id]}`}
+                subtitle="Todos los productos"
+            />
+
+            <ProductGrid products={products} />
+        </>
     )
 }
