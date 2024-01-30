09c177ad63c12f24f6333799f2cd133372af2eeb
cuter97 <aguscastets@gmail.com>
2024-01-30 19:25:22 -0300

    add cart, checkout, empty, orders, login and register screens

--- src/app/auth/new-acount/page.tsx
+++ src/app/auth/new-acount/page.tsx
@@ -3,17 +3,21 @@ import Link from 'next/link';
 
 export default function () {
     return (
-        <main className="flex flex-col min-h-screen pt-32 sm:pt-52">
+        <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
 
-            <h1 className={`${titleFont.className} text-4xl mb-5`}>Ingresar</h1>
+            <h1 className={`${titleFont.className} text-4xl mb-5`}>Nueva cuenta</h1>
 
             <div className="flex flex-col">
+
+                <label htmlFor="email">Nombre completo</label>
+                <input
+                    className="px-5 py-2 border bg-gray-300 rounded mb-5"
+                    type="text" />
 
                 <label htmlFor="email">Correo electrónico</label>
                 <input
                     className="px-5 py-2 border bg-gray-300 rounded mb-5"
                     type="email" />
-
 
                 <label htmlFor="email">Contraseña</label>
                 <input
@@ -21,13 +25,11 @@ export default function () {
                     type="email" />
 
                 <button
-
                     className="btn-primary">
-                    Ingresar
+                    Crear cuenta
                 </button>
 
-
-                {/* divisor l ine */}
+                {/* divisor line */}
                 <div className="flex items-center my-5">
                     <div className="flex-1 border-t border-gray-500"></div>
                     <div className="px-2 text-gray-800">O</div>
@@ -35,12 +37,12 @@ export default function () {
                 </div>
 
                 <Link
-                    href="/auth/new-account"
+                    href="/auth/login"
                     className="btn-secondary text-center">
-                    Crear una nueva cuenta
+                    Ingresar
                 </Link>
 
             </div>
-        </main>
+        </div>
     );
 }
